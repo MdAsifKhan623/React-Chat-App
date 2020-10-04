d2dbe30d80551cd24dd2f27b2685505f38a7ab66
Asif623 <mdasifk22@gmail.com>
2020-10-04 18:30:37 -0400

    changed the alignment

--- GraphQl/resolvers.js
+++ GraphQl/resolvers.js
@@ -62,6 +62,7 @@ module.exports={
                 //create user
                 newUser = new regUser({name,email,password,confirmPassword})
                 newUser.save()
+                //return user
                 return newUser
                 
             }catch(err){
