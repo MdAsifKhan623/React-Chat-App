88a6538da1afb43176c123a002e865c076d0266b
Asif623 <mdasifk22@gmail.com>
2021-01-13 01:01:40 -0500

    participant icon added

--- frontend/src/pages/ChatHome/ChatUsers.js
+++ frontend/src/pages/ChatHome/ChatUsers.js
@@ -50,8 +50,8 @@ export default function ChatUsers({setUserSelected, selectedUser}) {
     }
     return (
         <Col xs={2} md={4} className="px-0" style={{backgroundColor:'#e7d9ea'}}> 
-            <center>Participants</center>
-            <i className="fas fa-user-friends"></i>
+            <center><i className="fas fa-user-friends fa-2x mt-2"></i></center>
+            
             {usersTable}    
         </Col>
     )
