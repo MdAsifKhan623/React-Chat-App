bd7a3b9e2c2993472f19f79c13dc44119fb51d0b
Asif623 <mdasifk22@gmail.com>
2021-01-17 20:14:37 -0500

    subscription done

--- appserver.js
+++ appserver.js
@@ -5,6 +5,7 @@ const contextMiddleware =require('./Util/contextMiddleware')
 // const resolvers = require('./GraphQl/resolvers')
 const resolvers=require('./GraphQl/resolvers/resolverIndex')
 const typeDefs= require('./GraphQl/typeDefs')
+const {createServer} =require('http')
 
 const startServer = async () => {
   const app=express()
@@ -15,10 +16,14 @@ const startServer = async () => {
   });
 
   server.applyMiddleware({ app })
+
+  const httpServer = createServer(app)
+
+  server.installSubscriptionHandlers(httpServer)
   
   await mongoose.connect('mongodb://localhost:27017/test',{useNewUrlParser:true, useUnifiedTopology: true})
 
-  app.listen({port:4000},()=>{
+  httpServer.listen({port:4000},()=>{
     console.log(`Server started at http://localhost:4000${server.graphqlPath}`)
     console.log(`Server started at  ws://localhost:4000${server.subscriptionsPath}`)
   });
