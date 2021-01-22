b13ad1190f3b66a2f529fdfb9f0a234399a126a0
Asif623 <mdasifk22@gmail.com>
2021-01-21 22:37:57 -0500

    added some changes but the subscription does not work for some unknown reasons

--- Util/contextMiddleware.js
+++ Util/contextMiddleware.js
@@ -8,7 +8,6 @@ module.exports= (context) =>{
     let token
     if (context.req && context.req.headers.authorization){
         token=context.req.headers.authorization.split('Bearer ')[1]
-        console.log(token,'Novak')
         
     } else if(context.connection && context.connection.context.Authorization){
         
